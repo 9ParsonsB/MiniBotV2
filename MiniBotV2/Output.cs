6277641e3a0ee37810f3798b971517805b92409d
minijack <9parsonsb@gmail.com>
2015-02-12 14:29:23 +0000

    Initial commit.
    added gitignore

---
+++ MiniBotV2/Output.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using System.Threading.Tasks;
+
+namespace MiniBotV2
+{
+    class TextBoxStreamWriter : TextWriter
+    {
+        TextBox _output = null;
+        public TextBoxStreamWriter(TextBox output)
+        {
+            _output = output;
+        }
+
+        public override void Writer(char value)
+        {
+            base.Writer(value);
+            _output.AppendText(value.ToString());
+        }
+
+        public override Encoding Encoding
+        {
+            get { return System.Text.Enconding.UTF8; }
+        }
+
+    }
+
+
+}
